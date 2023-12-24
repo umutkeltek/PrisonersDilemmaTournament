a717b37ccec81b0a483cfc9b661c29f64140dee2
Umut Keltek <35880258+umutkeltek@users.noreply.github.com>
2023-12-24 23:09:52 +0300

    Some strategies added.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import random
+from sqlite3 import adapt
+import numpy as np
+
+
+import matplotlib.pyplot as plt
+
 
 # Define the play_round function to determine the outcome of a single round
 def play_round(strategy1, strategy2, history1, history2):
@@ -20,6 +26,16 @@ def tit_for_two_tats(history):
 def always_defect(_):
     return "defect"
 
+def pattern_recognition(history):
+    pattern_length = 3
+    if len(history) < pattern_length * 2:
+        return "cooperate"
+    last_pattern = history[-pattern_length:]
+    if history[-pattern_length*2:-pattern_length] == last_pattern:
+        return "defect" if last_pattern[-1] == "cooperate" else "cooperate"
+    return "cooperate"
+
+
 def always_cooperate(_):
     return "cooperate"
 
@@ -33,6 +49,13 @@ def grudger(history):
     if "defect" in history:
         return "defect"
     return "cooperate"
+
+def adaptive_strategy(history):
+    if not history:
+        return "cooperate"
+    cooperation_rate = history.count("cooperate") / len(history)
+    return "cooperate" if cooperation_rate > 0.5 else "defect"
+
 
 def pavlov(history):
     if not history:
@@ -91,8 +114,18 @@ def random_tit_for_tat(history):
         return "cooperate"
     return "defect" if random.random() < 0.05 else history[-1]
 
+def alternate_cooperate_defect(_):
+    return "cooperate" if random.choice([True, False]) else "defect"
+
+def mirror_last_two(history):
+    if len(history) < 2:
+        return "cooperate"
+    return "cooperate" if history[-1] == "cooperate" and history[-2] == "cooperate" else "defect"
+
+
 # Define a function to run a match between two strategies
-def run_match(strategy1, strategy2, rounds=100):
+def run_match(strategy1, strategy2, min_rounds=50, max_rounds=150):
+    rounds = random.randint(min_rounds, max_rounds)
     history1, history2 = [], []
     for _ in range(rounds):
         play_round(strategy1, strategy2, history1, history2)
@@ -113,13 +146,50 @@ def round_robin_tournament(strategies, rounds_per_match=100):
     
     return scores
 
+# Function to randomly change strategies
+def randomize_strategies(strategies, probability=0.1):
+    all_strategies = [
+        tit_for_tat, tit_for_two_tats, always_defect, always_cooperate, random_strategy,
+        suspicious_tit_for_tat, grudger, pavlov, gradual, probe, tester, firm_but_fair,
+        joss, downing, random_tit_for_tat, alternate_cooperate_defect, mirror_last_two,
+        pattern_recognition, adaptive_strategy
+    ]
+    for i in range(len(strategies)):
+        if random.random() < probability:
+            strategies[i] = random.choice(all_strategies)
+    return strategies
+
+
+# Function to simulate the tournament with strategy switching
+def simulate_tournament(strategies, rounds_per_match=100, switches=5):
+    results = np.zeros((len(strategies), len(strategies)))
+    for switch in range(switches):
+        strategies = randomize_strategies(strategies)
+        scores = round_robin_tournament(strategies, rounds_per_match)
+        for i, strategy1 in enumerate(strategies):
+            for j, strategy2 in enumerate(strategies):
+                if i != j:
+                    results[i, j] += scores[strategy1.__name__]
+    return results
+
 # List of strategies in the tournament
+
+# List and randomize strategies
 strategies = [
     tit_for_tat, tit_for_two_tats, always_defect, always_cooperate, random_strategy,
     suspicious_tit_for_tat, grudger, pavlov, gradual, probe, tester, firm_but_fair,
-    joss, downing, random_tit_for_tat
+    joss, downing, random_tit_for_tat, alternate_cooperate_defect, mirror_last_two,
+    pattern_recognition, adaptive_strategy
 ]
+strategies = randomize_strategies(strategies)
 
 # Run the tournament
-tournament_results = round_robin_tournament(strategies)
-print(tournament_results)
+tournament_results = simulate_tournament(strategies)
+
+# Plotting the results
+plt.imshow(tournament_results, cmap='hot', interpolation='nearest')
+plt.colorbar()
+plt.xticks(range(len(strategies)), [s.__name__ for s in strategies], rotation=45)
+plt.yticks(range(len(strategies)), [s.__name__ for s in strategies])
+plt.title("Strategy Performance in Iterated Prisoner's Dilemma")
+plt.show()
